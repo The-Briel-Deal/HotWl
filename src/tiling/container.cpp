d390c99305dbebf25924255036f57ccb150f078d
Gabriel Ford <gabe@fordltc.net>
2024-07-07 01:22:11 -0400

    Moved vert_split_containers to being a member function.

--- src/tiling/container.cpp
+++ src/tiling/container.cpp
@@ -1,5 +1,9 @@
 #include "container.hpp"
 #include "state.hpp"
+#include <includes.hpp>
+#include <output.hpp>
+#include <server.hpp>
+#include <xdg_shell.hpp>
 
 GfContainer::GfContainer(bool root, gfwl_tiling_state *state,
                          gfwl_container_type type,
@@ -25,3 +29,100 @@ enum gfwl_split_direction GfContainer::get_split_dir() {
     return GFWL_SPLIT_DIR_UNKNOWN;
   }
 }
+
+// TODO: Replace duplicate parts with generalized helpers.
+// Change this to get output size from the parent.
+void GfContainer::vert_split_containers() {
+  // Get count.
+  int count = this->child_containers.size();
+  if (count == 0) {
+    wlr_log(WLR_DEBUG, "You probably don't want to divide by 0");
+    return;
+  }
+
+  int width = this->box.width;
+  int height = this->box.height;
+
+  int per_win_height = height / count;
+
+  // Set all sizes. (recycling count for the index)
+  count = 0;
+  for (auto curr : this->child_containers) {
+    wlr_box box = {.x = this->box.x,
+                   .y = this->box.y + (per_win_height * count),
+                   .width = width,
+                   .height = per_win_height};
+    set_container_box(curr, box);
+    count += 1;
+  }
+}
+
+void hori_split_containers(struct std::shared_ptr<GfContainer> container) {
+  // Get count.
+  int count = container->child_containers.size();
+  if (count == 0) {
+    wlr_log(WLR_DEBUG, "You probably don't want to divide by 0");
+    return;
+  }
+  // Get Width and Height.
+  int width = container->box.width;
+  int height = container->box.height;
+
+  // Get per_win_width.
+  int per_win_width = width / count;
+
+  // Set all sizes. (recycling count for the index)
+  count = 0;
+  for (auto curr : container->child_containers) {
+    wlr_box box = {.x = container->box.x + (count * per_win_width),
+                   .y = container->box.y,
+                   .width = per_win_width,
+                   .height = height};
+    set_container_box(curr, box);
+    count += 1;
+  }
+}
+
+void split_containers(std::shared_ptr<GfContainer> container) {
+  switch (container->get_split_dir()) {
+  case GFWL_SPLIT_DIR_HORI:
+    hori_split_containers(container);
+    break;
+  case GFWL_SPLIT_DIR_VERT:
+    container->vert_split_containers();
+    break;
+  default:
+    break;
+  }
+}
+
+void parse_containers(std::shared_ptr<GfContainer> container) {
+  // Get output if we're at the root.
+  if (container->is_root) {
+    struct gfwl_output *output =
+        wl_container_of(container->server->outputs.next, output, link);
+    container->box.width = output->wlr_output->width;
+    container->box.height = output->wlr_output->height;
+  }
+  split_containers(container);
+  for (auto child : container->child_containers) {
+    if (child->e_type == GFWL_CONTAINER_HSPLIT ||
+        child->e_type == GFWL_CONTAINER_VSPLIT) {
+      parse_containers(child);
+    }
+  }
+}
+
+// Only supports toplevels for now.
+void set_container_box(std::shared_ptr<GfContainer> container,
+                       struct wlr_box box) {
+  container->box = box;
+  if (container->e_type == GFWL_CONTAINER_TOPLEVEL) {
+    struct wlr_xdg_toplevel *toplevel = container->toplevel->xdg_toplevel;
+    // Set the size.
+    wlr_xdg_toplevel_set_size(toplevel, box.width, box.height);
+    // Set the position.
+    struct wlr_scene_tree *scene_tree = (wlr_scene_tree *)toplevel->base->data;
+    wlr_scene_node_set_position(&scene_tree->node, box.x, box.y);
+  }
+};
--- src/tiling/container.hpp
+++ src/tiling/container.hpp
@@ -12,28 +12,34 @@ enum gfwl_container_type {
   GFWL_CONTAINER_ROOT = 4,
 };
 
-struct GfContainer {
+class GfContainer {
 public:
-  gfwl_split_direction get_split_dir();
-  struct gfwl_tiling_state *tiling_state = NULL;
-  gfwl_container_type e_type = GFWL_CONTAINER_UNKNOWN;
-  struct std::shared_ptr<GfContainer> parent_container = NULL;
-  bool is_root = false;
-  struct wlr_box box = {.x = 0, .y = 0, .width = 0, .height = 0};
-
-  struct gfwl_toplevel *toplevel = NULL;
-  struct gfwl_server *server = NULL;
-
-  std::vector<std::shared_ptr<GfContainer>> child_containers;
-
   GfContainer(bool root, gfwl_tiling_state *state, gfwl_container_type type,
               std::shared_ptr<GfContainer> parent, gfwl_server *server,
               gfwl_toplevel *toplevel);
+
+  // Member Functions
+  gfwl_split_direction get_split_dir();
+  void vert_split_containers();
+
+  // The Container Above and Below this one in the tiling tree
+  std::shared_ptr<GfContainer> parent_container = NULL;
+  std::vector<std::shared_ptr<GfContainer>> child_containers;
+
+  // Descriptors of this container
+  gfwl_container_type e_type = GFWL_CONTAINER_UNKNOWN;
+  bool is_root = false;
+
+  // The Dimensions of a container
+  wlr_box box = {.x = 0, .y = 0, .width = 0, .height = 0};
+
+  // Some relevant state
+  gfwl_tiling_state *tiling_state = NULL;
+  gfwl_toplevel *toplevel = NULL;
+  gfwl_server *server = NULL;
 };
 
 void hori_split_containers(std::shared_ptr<GfContainer> container);
-
-void vert_split_containers(std::shared_ptr<GfContainer> container);
 
 void set_container_box(std::shared_ptr<GfContainer> toplevel,
                        struct wlr_box box);
