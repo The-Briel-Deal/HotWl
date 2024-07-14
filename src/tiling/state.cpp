83de0efa9758da8034f299a57c07e24aa54c140a
Gabriel Ford <gabe@fordltc.net>
2024-07-14 04:09:30 -0400

    Removed some old unused methods.

--- src/tiling/state.cpp
+++ src/tiling/state.cpp
@@ -2,8 +2,9 @@
 #include <cassert>
 #include <memory>
 #include <server.hpp>
-#include <tiling/container.hpp>
-/* DEPRECATED: Moving to this being the containers responsibility.
+
+/* TODO: Remove.
+ * DEPRECATED: Moving to this being the containers responsibility.
  * Flips tiling states split_dir */
 void GfTilingState::flip_split_direction() {
   if (this->split_dir == GFWL_SPLIT_DIR_HORI)
@@ -14,15 +15,14 @@ void GfTilingState::flip_split_direction() {
 
 /* Once we make the call to the appropriate container, its up to the container
  * to put it in the right place. */
-void GfTilingState::insert(gfwl_toplevel *toplevel) {
+std::weak_ptr<GfContainer> GfTilingState::insert(gfwl_toplevel *toplevel) {
   /* Ideally insert into the active container in this tiling state. */
   auto focused_container = this->active_toplevel_container.lock();
   if (focused_container) {
-    focused_container->parent_container.lock()->insert_child(toplevel);
-    return;
+    return focused_container->parent_container.lock()->insert_child(toplevel);
   }
 
   /* In some cases (like when the last focused has been freed, we will insert
    * in the root node. */
-  this->root->insert_child(toplevel);
+  return this->root->insert_child(toplevel);
 }
--- src/tiling/state.hpp
+++ src/tiling/state.hpp
@@ -14,18 +14,11 @@ struct gfwl_toplevel;
 
 struct GfTilingState : public std::enable_shared_from_this<GfTilingState> {
 public:
-  void insert(std::weak_ptr<GfContainer> container);
-  void insert(gfwl_toplevel *toplevel);
+  /* Inserts a toplevel into the tiling state. */
+  std::weak_ptr<GfContainer> insert(gfwl_toplevel *toplevel);
 
-  void insert_child_container(std::shared_ptr<GfContainer> parent,
-                              std::shared_ptr<GfContainer> child);
-  void new_vert_split_container(std::shared_ptr<GfContainer> new_container,
-                                std::shared_ptr<GfContainer> focused_container);
-  void new_hori_split_container(std::shared_ptr<GfContainer> new_container,
-                                std::shared_ptr<GfContainer> focused_container);
   void flip_split_direction();
-  void reparent_container(std::shared_ptr<GfContainer> parent,
-                          std::shared_ptr<GfContainer> child);
+
   std::shared_ptr<GfContainer> root;
   std::weak_ptr<GfContainer> active_toplevel_container;
   gfwl_split_direction split_dir;
