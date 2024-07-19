6b21b252d4a28a805a8210b1068a5717e4f08b03
Gabriel Ford <gabe@fordltc.net>
2024-07-18 21:26:32 -0400

    Remove some old comments.

--- src/tiling/state.cpp
+++ src/tiling/state.cpp
@@ -26,19 +26,4 @@ std::weak_ptr<GfContainer> GfTilingState::insert(gfwl_toplevel *toplevel) {
   }
 
   return focused_container->insert(toplevel);
-  //   if (focused_container) {
-  //     return focused_container->insert_based_on_longer_dir(toplevel);
-  //   }
-  //
-  //   /* If the root already has its split container this insert into it. */
-  //   if (this->root->child_containers.size() > 0)
-  //     return this->root->child_containers[0]->insert_sibling(toplevel);
-  //
-  //   /* In some cases (like when the last focused has been freed), we will
-  //   insert
-  //    * in the root node. */
-  //   // TODO: I shoudn't be doing this. (Inserting into the root, it should be
-  //   // into the top hori-split.
-  //   return this->root->insert_child_in_split(toplevel,
-  //   GFWL_CONTAINER_HSPLIT);
 }
