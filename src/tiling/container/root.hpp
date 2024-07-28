212636dfc320e82a38c22fc8df1d0fb3d54176c5
Gabriel Ford <gabe@fordltc.net>
2024-07-28 15:37:45 -0400

    Change overrides to final and use move to base class.

--- src/tiling/container/root.hpp
+++ src/tiling/container/root.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <utility>
+
 #include "base.hpp"
 
 class GfContainerRoot : public GfContainer {
@@ -6,11 +8,11 @@ public:
   explicit GfContainerRoot(GfServer&                    server,
                            const gfwl_container_type    e_type,
                            std::weak_ptr<GfTilingState> tiling_state) :
-      GfContainer(server, e_type, tiling_state){};
+      GfContainer(server, e_type, std::move(tiling_state)){};
 
-  std::weak_ptr<GfContainer> insert(GfToplevel* to_insert);
-  void                       parse_containers();
-  void                       close();
+  std::weak_ptr<GfContainer> insert(GfToplevel* to_insert) final;
+  void                       parse_containers() final;
+  void                       close() final;
 
 private:
   void set_to_output_size();
