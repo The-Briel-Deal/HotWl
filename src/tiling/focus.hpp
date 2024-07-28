3f11cba2e3ca159c85463ab5420a3d5b8f785e49
Gabriel Ford <gabe@fordltc.net>
2024-07-28 15:29:40 -0400

    Remove unused headers.

--- src/tiling/focus.hpp
+++ src/tiling/focus.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-// This header describes the interface of how to change focus via keyboard.
-
 #include "state.hpp"
+
 #include <includes.hpp>
 #include <memory>
 #include <scene.hpp>
@@ -20,10 +19,7 @@ struct GfPoint {
   int y;
 };
 
-/*
- * Shift the focus to the next window in any of 4 directions.
- */
-bool       tiling_focus_move_in_dir(enum gfwl_tiling_focus_direction      dir,
-                                    const std::shared_ptr<GfTilingState>& state);
+bool    tiling_focus_move_in_dir(enum gfwl_tiling_focus_direction      dir,
+                                 const std::shared_ptr<GfTilingState>& state);
 
 GfPoint get_container_origin(const std::shared_ptr<GfContainer>& container);
